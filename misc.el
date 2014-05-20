33d052e03733206f7ba2f04f6e3efd4fca2f95a4
Adam Stokes <adam.stokes@ubuntu.com>
2014-05-19 21:33:35 -0400

    update fon
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- misc.el
+++ misc.el
@@ -50,7 +50,7 @@
 
 ;; Set font size
 (set-face-attribute 'default nil :height 120)
-(add-to-list 'default-frame-alist '(font . "Ubuntu Mono-12"))
+(add-to-list 'default-frame-alist '(font . "Inconsolata-14"))
 
 ;; Do not ask for confirmation
 (setq confirm-nonexistent-file-or-buffer nil)
