1c14148dc69832b635fb8cd8de1d82fc2e764ed3
Adam Stokes <adam.stokes@ubuntu.com>
2014-09-04 16:39:24 -0400

    update font
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- misc.el
+++ misc.el
@@ -50,7 +50,7 @@
 
 ;; Set font size
 (set-face-attribute 'default nil :height 120)
-(add-to-list 'default-frame-alist '(font . "Inconsolata-14"))
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-14"))
 
 ;; Do not ask for confirmation
 (setq confirm-nonexistent-file-or-buffer nil)
